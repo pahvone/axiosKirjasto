e4d816731d64641173aec04a8a6ada669e1e1953
Ville Pelimanni <ville.pelimanni@gmail.com>
2022-10-17 20:04:05 +0300

    palautettava

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const URL = 'https://api.jokes.one/jod';
 
 function App() {
+  const [title,setTitle] = useState('');
+  const [text,setText] = useState('');
+
+  useEffect (() => {
+    axios.get(URL)
+      .then((response)=> {
+        const joke = response.data.contents.jokes[0].joke;
+        setTitle(joke.title);
+        setText(joke.text);
+      }).catch (error => {
+        alert(error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div /*style={{margin: 50}}*/>
+      <h1 class="p-3 mb-2 bg-primary text-white"><span class="header">Joke of the day</span></h1>
+      <h3><span class="title">{title}</span></h3>
+      <p><span class="punchline">{text}</span></p>
     </div>
+    </>
   );
 }
 
